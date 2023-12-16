247943860b4750e3570c682a7ee95ff0f671a6f2
FerGuHe <143969875+FerGuHe@users.noreply.github.com>
2023-12-16 15:24:34 -0600

    extension

--- Extensión de Navegador/start/dist/background.js
+++ Extensión de Navegador/start/dist/background.js
@@ -1,9 +1,4 @@
 //add listener here
-
-//borrowed from energy lollipop extension
-//draw the icon here
-
-
 chrome.runtime.onMessage.addListener(function (msg, sender,
     sendResponse) {
     if (msg.action === 'updateIcon') {
@@ -11,8 +6,10 @@ chrome.runtime.onMessage.addListener(function (msg, sender,
     drawIcon(msg.value) });
     }
     });
-    //tomado de la extensión Energy Lollipop, ¡buena característica!
-    function drawIcon(value) {
+//borrowed from energy lollipop extension
+//draw the icon here
+//tomado de la extensión Energy Lollipop, ¡buena característica!
+function drawIcon(value) {
     let canvas = document.createElement('canvas');
     let context = canvas.getContext('2d');
     context.beginPath();
@@ -21,21 +18,3 @@ chrome.runtime.onMessage.addListener(function (msg, sender,
     context.fill();
     return context.getImageData(50, 50, 100, 100);
     }
-
-    chrome.runtime.onMessage.addListener(function (msg, sender,
-        sendResponse) {
-        if (msg.action === 'updateIcon') {
-        chrome.browserAction.setIcon({ imageData:
-        drawIcon(msg.value) });
-        }
-        });
-        //tomado de la extensión Energy Lollipop, ¡buena característica!
-        function drawIcon(value) {
-        let canvas = document.createElement('canvas');
-        let context = canvas.getContext('2d');
-        context.beginPath();
-        context.fillStyle = value.color;
-        context.arc(100, 100, 50, 0, 2 * Math.PI);
-        context.fill();
-        return context.getImageData(50, 50, 100, 100);
-        }  
